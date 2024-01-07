706936ca63b3cf2fe7a6e1b8e59c8f669807fcfc
Robert Spencer <spencer.robert.d@gmail.com>
2024-01-07 16:32:17 -0700

    adds basic vars to world

--- world.py
+++ world.py
@@ -1,6 +1,14 @@
 from city import City
 from biome import Biome
 class World:
-    def __init__(self) -> None:
-        pass
+    def __init__(self, name="", cities=[], biomes=[], landX=0, landY=0) -> None:
+        self.name = name
+        self.cities = cities
+        self.biomes = biomes
+        worldbox = []
+        for Y in range(landY):
+            worldbox.append([])  # adds an empty box into the worldbox to fill with biomes
+            for X in range(landX):
+                worldbox[Y].append(Biome())  # inserts an empty biome
+      
         
