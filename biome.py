e68143f6def04d1f281dd5dcdf3accec98fb7400
Robert Spencer <spencer.robert.d@gmail.com>
2024-01-07 16:44:02 -0700

    adds name and symbol vars

--- biome.py
+++ biome.py
@@ -1,3 +1,7 @@
 class Biome:
-    def __init__(self) -> None:
-        pass
+    def __init__(self, name="empty") -> None:
+        self.name = name
+        if self.name == "empty":
+            self.symbol = "X"
+        else:
+            self.symbol = ""
